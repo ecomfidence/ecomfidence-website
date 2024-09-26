3de1dd43bbf688d7962e44ea1eb23d4c39e43f1f
Ahmed Raza Gigani <arj9999blast@gmail.com>
2024-09-26 22:19:23 +0500

    Create Pages

--- app/page.js
+++ app/page.js
@@ -1,38 +1,31 @@
 import Layout from "@/components/layout/Layout"
 import Blog1 from "@/components/sections/Blog1"
-import CaseStudy1 from "@/components/sections/CaseStudy1"
 import HeroEcom1 from "@/components/sections/HeroEcom1"
 import ProductEcom from "@/components/sections/ProductEcom"
-import Service1 from "@/components/sections/Service1"
-import Team1 from "@/components/sections/Team1"
-import Testimonial1 from "@/components/sections/Testimonial1"
-import TextSLider1 from "@/components/sections/TextSLider1"
-import TextSLider2 from "@/components/sections/TextSLider2"
+import ServiceEcom from "@/components/sections/ServiceEcom"
 import TextSLider3 from "@/components/sections/TextSLider3"
 import TextSLiderEcom from "@/components/sections/TextSLiderEcom"
-import Watch1 from "@/components/sections/Watch1"
 import OurPartners from "@/components/sections/OurPartners"
+import StepWorkingEcom from "@/components/sections/StepWorkingEcom"
+import ProtfolioEcom from "@/components/sections/ProtfolioEcom"
+import TeamEcom from "@/components/sections/TeamEcom"
+import Testimonial4 from "@/components/sections/Testimonial4"
 export default function Home() {
 
     return (
         <>
 
-            <Layout headerStyle={2} footerStyle={1}>
+            <Layout headerStyle={2} footerStyle={5}>
                 <div className="position-relative overflow-hidden">
                     <div className="line-shape cus-z-1 first w-100 h-100 d-flex flex-wrap" />
                     <HeroEcom1 />
                     <TextSLiderEcom />
                     <ProductEcom />
                     <OurPartners />
-                    <Service1 />
-                    <TextSLider1 />
-                    <CaseStudy1 />
-                    <Watch1 />
-                    <Team1 />
-                    <TextSLider2 />
-                    <Testimonial1 />
-                    <Blog1 />
-                    <TextSLider3 />
+                    <ServiceEcom />
+                    <StepWorkingEcom />
+                    <ProtfolioEcom />
+                    <TeamEcom />
                 </div>
             </Layout>
         </>
