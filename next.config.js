1edc8bb50497e4a8a5a3ca739745951d9081d5b1
Ahmed Raza Gigani <arj9999blast@gmail.com>
2024-09-27 11:33:00 +0500

    Output Setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export', // Ensures that Next.js exports static files
+    trailingSlash: true, // Optional: Adds trailing slashes to all URLs (e.g., `/about/` instead of `/about`)
+  }
+  
+  module.exports = nextConfig
+  
 
-module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
